6fe3af1c284c6613a0da9785410d60f942f25638
Michal Mrozek <m.mrozek@hotmail.com>
2022-10-02 16:16:00 +0100

    comparators removed, date api used for represent the date.

--- src/library/model/Magazine.java
+++ src/library/model/Magazine.java
@@ -1,37 +1,29 @@
 package library.model;
 
+import java.time.MonthDay;
 import java.util.Objects;
 
 public class Magazine extends Publication {
 public static final String TYPE = "Magazine";
-    private int month;
-    private int day;
+
+    private MonthDay monthDay;
     private String language;
 
     public Magazine(String title, String publisher, String language, int year, int month, int day) {
         super(title, publisher, year);
-        this.month = month;
-        this.day = day;
         this.language = language;
+        this.monthDay = MonthDay.of(month,day);
 
 
     }
 
 
-    public int getMonth() {
-        return month;
+    public MonthDay getMonthDay() {
+        return monthDay;
     }
 
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
+    public void setMonthDay(MonthDay monthDay) {
+        this.monthDay = monthDay;
     }
 
     public String getLanguage() {
@@ -44,7 +36,7 @@ public static final String TYPE = "Magazine";
 
     @Override
     public String toString() {
-        return super.toString() + "-" + month + "-" + day + "; " + language;
+        return super.toString() + "-" + monthDay.getMonthValue() + "-" + monthDay.getDayOfMonth() + "; " + language;
     }
 
     @Override
@@ -53,10 +45,11 @@ public static final String TYPE = "Magazine";
                 getTitle() + ";" +
                 getPublisher() + ";" +
                 getYear() + ";" +
-                month + ";" +
-                day + ";" +
+                monthDay.getMonthValue() + ";" +
+                monthDay.getDayOfMonth() + ";" +
                 language + "";
     }
+
 
     @Override
     public boolean equals(Object o) {
@@ -64,11 +57,11 @@ public static final String TYPE = "Magazine";
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Magazine magazine = (Magazine) o;
-        return month == magazine.month && day == magazine.day && Objects.equals(language, magazine.language);
+        return Objects.equals(monthDay, magazine.monthDay) && Objects.equals(language, magazine.language);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), month, day, language);
+        return Objects.hash(super.hashCode(), monthDay, language);
     }
 }
--- src/library/model/Publication.java
+++ src/library/model/Publication.java
@@ -1,24 +1,25 @@
 package library.model;
 
 import java.io.Serializable;
+import java.time.Year;
 import java.util.Objects;
 
 public abstract class Publication implements Serializable,Comparable<Publication>, CsvConvertible {
-    private int year;
+    private Year year;
     private String title;
     private String publisher;
 
      Publication(String title, String publisher, int year) {
-        this.year = year;
+        this.year = Year.of(year);
         this.title = title;
         this.publisher = publisher;
     }
 
-    public int getYear() {
+    public Year getYear() {
         return year;
     }
 
-    public void setYear(int year) {
+    public void setYear(Year year) {
         this.year = year;
     }
 
@@ -49,7 +50,7 @@ public abstract class Publication implements Serializable,Comparable<Publication
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Publication that = (Publication) o;
-        return year == that.year && Objects.equals(title, that.title) && Objects.equals(publisher, that.publisher);
+        return Objects.equals(year, that.year) && Objects.equals(title, that.title) && Objects.equals(publisher, that.publisher);
     }
 
     @Override
